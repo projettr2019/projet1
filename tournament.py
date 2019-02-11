8624cc5c65a2d3b50b5a7a294e11ea8c04a54937
thomas baker <thomas.compteenplus@gmail.com>
2019-02-11 16:14:24 +0100

    seance4

--- tournament.py
+++ tournament.py
@@ -22,13 +22,14 @@ def get_team(nb_players):
     if nb_players == 2:
         team.add("defence1",DefenceStrategy())
         team.add("fonceur2",FonceStrategy())
+        
     return team
     
-if __name__ == " __main__ ":
-# from soccersimulator import Simulation, show_simu
+if __name__ == "__main__":
+#from soccersimulator import Simulation, show_simu
 # Check teams with 1 player and 2 players
-    team1 = get_team(2)
-    team2 = get_team(2)
+    team1 = get_team(1)
+    team2 = get_team(1)
 # Create a match
     simu = Simulation( team1 , team2 )
 # Simulate and display the match
